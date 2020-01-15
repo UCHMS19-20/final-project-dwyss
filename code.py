c18e445edf3159d0fc777c45bdcf46413c033e20
dwyss1 <dwyss@ucvts.org>
2020-01-15 10:18:43 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,3 @@
-# loading pygame
-import sys
-import pygame
-
-#initiate the pygame
-pygame.init()
-
-# size of pygame window
-screen = pygame.display.set_mode( (800, 450) )
-
-# this doesn't work right now cause i still have to add a lot of stuff
-
 # these are the set of rules/the introduction that will show up on the screen when the game is starting
 print("Let's play baseball!!!")
 print("Here are the rules:\
@@ -25,43 +13,107 @@ print("Here are the rules:\
     p=power_swing\
     c=contact_swing\
     x=no_swing\
-    now, you should be able to play!!!")
+    Now, you should be able to play!!!")
 
-pitcher_choices = ['f', 'v', 'e', 's']
-hitter_choices = ['n', 'p', 'c', 'x']
+def get_pitch_type():
+    """Return the type of pitch that the pitcher will select"""
+    pitcher_choices = ['f', 'v', 'e', 's']
+    pitch_type = input(pitcher_choices)
+    return
 
-pitch_type = input(pitcher_choices)
-hit_type = input(hit_type)
+get_pitch_type()
 
-# load the image of the baseball field
-background_image = pygame.image.load(baseball_field.png).convert()
+def get_hit_type(hit):
+    """Return the type of hit that the hitter will select"""
+    hitter_choices = ['n', 'p', 'c', 'x']
+    hit_type = input(hitter_choices)
+    return
+
+# following code used as comment temporarily
+# so that it can still be in code but will not be used for now
+#pitcher_choices = ['f', 'v', 'e', 's']
+#hitter_choices = ['n', 'p', 'c', 'x']
+#pitch_type = input(pitcher_choices)
+#hit_type = input(hit_type)
+
+# these are the various outcomes and results
+# different for each combination of input for the two players
+if pitch_type == 'f':
+    if hit_type == 'n':
+        print("Strike swinging!")
+        strikes += 1
+    elif hit_type == 'p':
+        print("Home run!")
+        bases += 4
+    elif hit_type == 'c':
+        print("Single!")
+        bases += 1
+    elif hit_type == 'x':
+        print("Stike looking!")
+        strikes += 1
+elif pitch_type == 'v':
+    if hit_type == 'n':
+        print("Groundout!")
+        outs += 1
+    elif hit_type == 'p':
+        print("Strike swinging!")
+        strikes += 1
+    elif hit_type == 'c':
+        print("Single!")
+        bases += 1
+    elif hit_type == 'x':
+        print("Ball!")
+        balls += 1
+elif pitch_type == 'e':
+    if hit_type == 'n':
+        print("Double!")
+        bases += 2
+    elif hit_type == 'p':
+        print("Home run!")
+        bases += 4
+    elif hit_type == 'c':
+        print("Groundout!")
+        outs += 1
+    elif hit_type == 'x':
+        print("Ball!")
+        balls += 1
+elif pitch_type == 's':
+    if hit_type == 'n':
+        print("Lineout!")
+        outs += 1
+    elif hit_type == 'p':
+        print("Strike swinging!")
+        strikes += 1
+    elif hit_type == 'c':
+        print("Single!")
+        bases += 1
+    elif hit_type == 'x':
+        print("Ball!")
+        balls += 1
 
 # all of the data
-ball = 0
+balls = 0
+strikes = 0
+outs = 0
+bases = 0
+away_team_runs = 0
+home_team_runs = 0
 # definition of a walk
-while ball == 4:
-    base += 1
-strike = 0
-while strike == 3:
-    out += 1
-out = 0
-while out == 3:
-    sys.exit()
-
-# load the turtle, which will be the path that the baserunners take
-import turtle as t
-turtle.speed(8)
-# turtle.pos(-,-)
-# above is not finished, i will have to go through trial end error so i can insert the exact points
-
-t.rt(-45)
-
-def bases():
-    """This is how the runners will advance"""
-    
-    for n in range(base):
-        t.fd(50)
-        t.rt(-90)
+while balls == 4:
+    bases += 1
+    balls = 0
+    strikes = 0
+while strikes == 3:
+    outs += 1
+    balls = 0
+    strikes = 0
+while outs == 3:
+    # change sides
+    balls = 0
+    strikes = 0
+    outs = 0
+while bases == 4:
+    Team += 1
 
 # this is where the various abilities that the players have are stored
 class players:
@@ -73,58 +125,3 @@ class players:
         hitter = players("Batter Boy", "normal swing", "power swing", "contact swing", "no swing")
 
 code.interact(local=locals())
-
-# these are the various outcomes and results
-# different for each combination of input for the two players
-if pitch_type == 'f':
-    if hit_type == 'n':
-        print("Strike swinging!")
-        strike += 1
-    elif hit_type == 'p':
-        print("Home run!")
-        base += 4
-    elif hit_type == 'c':
-        print("Single!")
-        base += 1
-    elif hit_type == 'x':
-        print("Stike looking!")
-        strike += 1
-elif pitch_type == 'v':
-    if hit_type == 'n':
-        print("Groundout!")
-        out += 1
-    elif hit_type == 'p':
-        print("Strike swinging!")
-        strike += 1
-    elif hit_type == 'c':
-        print("Single!")
-        base += 1
-    elif hit_type == 'x':
-        print("Ball!")
-        ball += 1
-elif pitch_type == 'e':
-    if hit_type == 'n':
-        print("Double!")
-        base += 2
-    elif hit_type == 'p':
-        print("Home run!")
-        base += 4
-    elif hit_type == 'c':
-        print("Groundout!")
-        out += 1
-    elif hit_type == 'x':
-        print("Ball!")
-        ball += 1
-elif pitch_type == 's':
-    if hit_type == 'n':
-        print("Lineout!")
-        out += 1
-    elif hit_type == 'p':
-        print("Strike swinging!")
-        strike += 1
-    elif hit_type == 'c':
-        print("Single!")
-        base += 1
-    elif hit_type == 'x':
-        print("Ball!")
-        ball += 1
